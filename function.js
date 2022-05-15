fb63aaec3393085f3819e5a9b13c43edc98de9e8
KristinaBal <105514019+KristinaBal@users.noreply.github.com>
2022-05-15 22:46:56 +0200

    toUpperCase

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-let array = ['one', 'du', 'three', 'five', 'six']
+let array = ['A', 'three', 'five', 'six']
 
 function getArrayValues(array) {
 
@@ -8,11 +8,11 @@ function getArrayValues(array) {
 
         for (let i = 0; i < array.length; i++) {
 
-            if (i === 0) {
+            if (i === array.length - 3) {
                 values.push(array[i])
             }
 
-            if (i === array.length - 1) {
+            if (i === array.length - 2) {
                 values.push(array[i])
             }
         }
@@ -24,22 +24,26 @@ function getArrayValues(array) {
 const convertToString = () => {
 
     let newArray = getArrayValues(array)
-
+    console.log(newArray)
     let string = newArray.toString()
-
+    console.log(string);
     let newString = ''
 
     for (let i = 0; i < string.length; i++) {
 
-        if (string[i] !== ',') {
+        // if (string[i] !== ',') {
 
-            newString += string[i]
-
+        //     newString += string[i]
+        // }
+        if (i === 0) {
+            newString = string[i].toUpperCase()
         }
-
     }
+    string[0] = newString
+    console.log(string[0]);
+    console.log(newString);
 
-    return newString
+    return string
 
 }
 
